51721f41af5776237223d8e906857185399a4f62
Lofanmi <lofanmi@gmail.com>
2021-08-29 12:10:41 +0000

    feat(datetime): math

--- datetime/datetime.go
+++ datetime/datetime.go
@@ -8,7 +8,7 @@ import (
 
 /*
 #cgo CFLAGS: -I./timelib
-#cgo LDFLAGS: -L${SRCDIR}/timelib -lastro -linterval -lparsedate -lparseisointervals -lparsetz -ltimelib -ltm2unixtime -lunixtime2tm -ldow
+#cgo LDFLAGS: -L${SRCDIR}/timelib -lastro -linterval -lparsedate -lparseisointervals -lparsetz -ltimelib -ltm2unixtime -lunixtime2tm -ldow -lm
 #include "time.h"
 #include "timelib.h"
 timelib_tzinfo *_date_parse_tzfile_wrapper(const char *formal_tzname, const timelib_tzdb *tzdb, int *dummy_error_code)
